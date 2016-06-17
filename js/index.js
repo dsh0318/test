1e4614a3cc81e5c610f567f4ce571976a8b38451
dsh0318 <928814276@qq.com>
2016-06-17 21:26:21 +0800

    简历

--- js/index.js
+++ js/index.js
@@ -20,8 +20,8 @@
         onSlideNextEnd: function () {
             step++;
             change();
-            if (step == 6) {
-                step = 0;
+            if (step ==6 ) {
+                step =1;
             }
         }
     });
@@ -33,8 +33,6 @@
             curDiv.id = index === step ? curDiv.getAttribute("trueId") : null;
         });
     }
-
-    //->给区域增加一个loop:true的时候,会自己往开头和结尾各增加一张一模一样的,但是我还需要把区域定位到“真实的第一张”,所以开始会自己向下切换一次,我们让初始的step=0即可
 }();
 
 //->音频的自动播放
@@ -64,14 +62,3 @@
         audioBox.className = "audio";
     }, false);
 }();
-
-
-
-
-
-
-
-
-
-
-
